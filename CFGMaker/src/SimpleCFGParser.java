a9c3074d4086f6df3c03aabfa3fbb61faba8dbdd
meeby <meeby@192.168.100.24>
2016-07-18 19:43:13 -0600

    Building simple basic blocks from expression statements

--- CFGMaker/src/SimpleCFGParser.java
+++ CFGMaker/src/SimpleCFGParser.java
@@ -9,6 +9,7 @@ import com.github.javaparser.ast.CompilationUnit;
 import com.github.javaparser.ast.Node;
 import com.github.javaparser.ast.stmt.BlockStmt;
 import com.github.javaparser.ast.stmt.ExpressionStmt;
+import com.github.javaparser.ast.stmt.IfStmt;
 import com.github.javaparser.ast.stmt.Statement;
 import com.github.javaparser.ast.visitor.GenericVisitorAdapter;
 
@@ -28,8 +29,9 @@ public class SimpleCFGParser {
 			CompilationUnit cu = JavaParser.parse(in);
 			in.close();
 			BlockVisitor blockVisitor = new BlockVisitor();
-//			Node result = (Node) blockVisitor.visit(cu, null);
 			this.cfg = blockVisitor.returnCFG(cu, null);
+//			StatementVisitor statementVisitor = new StatementVisitor();
+//			statementVisitor.returnCFG(cu, null);
 //			System.out.println("----------Main:\n"+result.toStringWithoutComments()+"\n");
 		} catch (FileNotFoundException e) {
 			// TODO Auto-generated catch block
@@ -53,32 +55,80 @@ public class SimpleCFGParser {
 	    @Override
 	    public Object visit(BlockStmt n, Object arg) {
 	    	List<Node> children = n.getChildrenNodes();
-	    	n.toStringWithoutComments();
+	    	System.out.println("Looking at: "+n.getBeginLine()+"     Children: "+children.size()+"\n"+n.toString()+"\n");
 	    	//adds the current BlockStmt's children to the CFG if the children do not have children themselves
-	    	if(children.size()>1){
-	    		String basicBlock = "";
-	    		int basicBlockBegin = -1;
-	    		int basicBlockEnd = -1;
-	    		for(int i=0; i<children.size(); i++){
-	    			Node child = children.get(i);
-	    			int begin = child.getBeginLine();
-    	    		int end = child.getEndLine();
-    	    		String code = child.toStringWithoutComments();
-	    			if(child.getChildrenNodes().size()==1){
-	    				if(basicBlockBegin==-1){
-	    					basicBlockBegin = begin;
-	    					basicBlockEnd = end;
-	    					basicBlock += code+"\n";
-	    				}
-	    				else{
-	    					basicBlockEnd = end;
-	    					basicBlock += code+"\n";
-	    					this.cfg.addNode(basicBlockBegin, basicBlockEnd, basicBlock);
-	    					basicBlock = "";
-	    		    		basicBlockBegin = -1;
-	    		    		basicBlockEnd = -1;
-	    				}
-//	    	    		this.cfg.addNode(begin, end, code);
+//	    	if(children.size()>1){
+//	    		String basicBlock = "";
+//	    		int basicBlockBegin = -1;
+//	    		int basicBlockEnd = -1;
+//	    		boolean newBlockNeeded = false;
+//	    		for(int i=0; i<children.size(); i++){
+//	    			Node child = children.get(i);
+//	    			int begin = child.getBeginLine();
+//    	    		int end = child.getEndLine();
+//    	    		String code = child.toStringWithoutComments();
+//	    			if(child.getChildrenNodes().size()==1){
+////	    				if(basicBlockBegin==-1){
+////	    					basicBlockBegin = begin;
+////	    					basicBlockEnd = end;
+////	    					basicBlock += code+"\n";
+////	    				}
+////	    				else{
+////	    					basicBlockEnd = end;
+////	    					basicBlock += code+"\n";
+////	    					this.cfg.addNode(basicBlockBegin, basicBlockEnd, basicBlock);
+////	    					basicBlock = "";
+////	    		    		basicBlockBegin = -1;
+////	    		    		basicBlockEnd = -1;
+////	    				}
+////	    	    		this.cfg.addNode(begin, end, code);
+////	    				System.out.println("Line: "+child.getBeginLine()+" "+child.getClass());
+////	    				basicBlockBegin = begin;
+//	    				if(basicBlockBegin == -1){
+//	    					basicBlockBegin = begin;
+//	    				}
+//    					basicBlockEnd = end;
+//    					basicBlock += code+"\n";
+//	    			}
+//	    			else{
+//	    				if(basicBlock != ""){
+//		    				this.cfg.addNode(basicBlockBegin, basicBlockEnd, basicBlock);
+//		    				basicBlock = "";
+//	    		    		basicBlockBegin = -1;
+//	    		    		basicBlockEnd = -1;
+//	    				}
+//	    			}
+//	    		}
+//	    	}
+	    	int begin = -1;
+	    	int end = -1;
+	    	String code = "";
+	    	boolean newNodeMade = false;
+	    	for(int i=0; i<children.size(); i++){
+	    		Node child = children.get(i);
+	    		System.out.println("Child begin: "+child.getBeginLine()+" Class: "+child.getClass());
+	    		if(child instanceof ExpressionStmt){
+		    		if(begin == -1){
+		    			begin = child.getBeginLine();
+		    		}
+		    		end = child.getEndLine();
+		    		code += child.toStringWithoutComments()+"\n";
+	    		}
+	    		else if(child instanceof IfStmt){
+	    			if(code!=""){
+		    			this.cfg.addNode(begin, end, code);
+		    			this.cfg.addNode(child.getBeginLine(), child.getEndLine(), "if("+((IfStmt) child).getCondition().toStringWithoutComments()+")");
+		    			begin = -1;
+		    			end = -1;
+		    			code = "";
+	    			}
+	    		}
+	    		else{
+	    			if(code!=""){
+		    			this.cfg.addNode(begin, end, code);
+		    			begin = -1;
+		    			end = -1;
+		    			code = "";
 	    			}
 	    		}
 	    	}
@@ -86,6 +136,21 @@ public class SimpleCFGParser {
 	    }
 
 		public CFG returnCFG(CompilationUnit cu, Object arg){
+	    	visit(cu, arg);
+	    	return this.cfg;
+	    }
+	}
+	
+	private static class StatementVisitor extends GenericVisitorAdapter<Object, Object>{
+		private CFG cfg = new CFG();
+		
+	    @Override
+	    public Object visit(ExpressionStmt s, Object arg) {
+	    	System.out.println("Begin: "+s.getBeginLine()+"\nCode:\n"+s.toStringWithoutComments()+"\nEnd: "+s.getEndLine()+"\n");
+	        return super.visit(s, arg);
+	    }
+	    
+	    public CFG returnCFG(CompilationUnit cu, Object arg){
 	    	visit(cu, arg);
 	    	return this.cfg;
 	    }
