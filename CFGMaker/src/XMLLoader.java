cbc09e85b311b790d2b88b8e19cded8a614e541e
calvoluis <lacalvo@miners.utep.edu>
2016-07-24 20:47:36 -0600

    Added class that adds XML file reading
    This class needs the use of three JAR files: xmlpull-1.1.3.1, xpp3_min-1.1.4c, and xstream-1.4.9. This asks the user for the file name and path and converts the xml file to a CFG object

---
+++ CFGMaker/src/XMLLoader.java
@@ -0,0 +1,34 @@
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+import com.thoughtworks.xstream.XStream;
+
+public class XMLLoader {
+	public static void main(String[] args){
+		BufferedReader userInput = new BufferedReader(new InputStreamReader(System.in));
+		System.out.println("Please enter file name (full path) for the XML:");
+		String fileName = "";
+		try {
+			boolean fileFound = false;
+			do{
+				fileName = userInput.readLine();
+				File file = new File(fileName);
+				if(file.isFile()){
+					System.out.println("File exists");
+					fileFound = true;
+				}
+				else{
+					System.out.println("File doesn't exist\nPlease enter file name (full path):");
+				}
+			} while(!fileFound);
+		} catch (IOException e){
+			e.printStackTrace();
+		}
+		File file = new File(fileName);
+		XStream xstream = new XStream();
+		CFG cfg =(CFG)xstream.fromXML(file);
+		cfg.printNodes();	
+	}
+}
