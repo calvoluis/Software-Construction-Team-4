92db149ee51ba9e4e28722e6c3175d82855f8f98
meeby <meeby@miners.utep.edu>
2016-07-25 22:24:20 -0600

    Adding final changes made to SimpleCFGParser

--- CFGMaker/src/SimpleCFGParser.java
+++ CFGMaker/src/SimpleCFGParser.java
@@ -1,7 +1,6 @@
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
-import java.util.List;
 
 import com.github.javaparser.JavaParser;
 import com.github.javaparser.ParseException;
